abaf0ca230bc619cc3d03920aca97cbaeddfa848
codecrafted1 <khushiambastha16@gmail.comn>
2026-09-13 16:08:46 +0530

    delete node in singlyLL

---
+++ 04-Linked-List/q.java
@@ -0,0 +1,14 @@
+public class q{
+    static class SinglyLinkedListNode{
+
+        int data;
+        SinglyLinkedListNode next;
+
+        SinglyLinkedListNode (int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+    
+}
